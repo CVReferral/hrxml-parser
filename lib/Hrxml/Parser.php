3861b496c1e6e040240fe0bd1ab0bd5f1d0d97ca
Khanh Tran <khanh.tq@geekup.vn>
2016-12-01 15:49:35 +0700

    Try to parse date in Vietname style.

--- lib/Hrxml/Parser.php
+++ lib/Hrxml/Parser.php
@@ -15,7 +15,7 @@ class Parser {
 
   public function parseFile($uri) {
     $xmlString = file_get_contents($uri);
-    return $this->parse($xmlString);
+    return $this->parseString($xmlString);
   }
 
   public function parseString($xmlString) {
@@ -34,16 +34,31 @@ class Parser {
 		}
 		$dob = self::getNodeValue($xml, 'DateOfBirth');
 		if ($dob) {
-			$candidate->birth_year = date('Y', strtotime($dob));
+			$dobTime = self::parseDateString($dob, '/');
+			if (!$dobTime) {
+				$dobTime = self::parseDateString($dob, '-');
+			}
+			if ($dobTime) {
+				$candidate->birth_year = date('Y', $dobTime);
+			}
 		}
-		$candidate->gender = null;
-		//t		here is no gener field
-		    $candidate->avatar = self::getNodeValue($xml->CandidateImage, 'CandidateImageData');
-		//T		ODO: should store to S3
+		$gender = strtolower(self::getNodeValue($xml, 'Gender'));
+		switch ($gender) {
+			case 'female':
+				$gender = 'Nữ';
+				break;
+			case 'male':
+				$gender = 'Nam';
+				break;
+		}
+		$candidate->sex = $gender;
+
+		//TODO: should store to S3
+		$candidate->avatar = self::getNodeValue($xml->CandidateImage, 'CandidateImageData');
 		
 		// 		Candidate Education
-		    // 		EducationSplit block repeats as many as value found in resume for it
-		    $candidate->education_list = [];
+		// 		EducationSplit block repeats as many as value found in resume for it
+		$candidate->education_list = [];
 		$i = 0;
 		foreach ($xml->SegregatedQualification->EducationSplit as $xmlEdu) {
 			$edu = new \StdClass;
@@ -57,7 +72,7 @@ class Parser {
 		}
 		
 		// 		Candidates Working Experience Drilldown
-		    // 		$candidate->working_list = [];
+		// 		$candidate->working_list = [];
 		$i = 0;
 		foreach ($xml->SegregatedExperience->WorkHistory as $xmlWork) {
 			$work = new \StdClass;
@@ -66,6 +81,30 @@ class Parser {
 			$work->position = self::getNodeValue($xmlWork, 'JobProfile');
 			$work->description = self::getNodeValue($xmlWork, 'JobDescription');
 			
+			$startDate = self::getNodeValue($xmlWork, 'StartDate');
+			if ($startDate) {
+				$startDateTime = self::parseDateString($startDate, '/');
+			}
+			if (!$startDateTime) {
+				$startDateTime = self::parseDateString($startDate, '-');
+			}
+			if ($startDateTime) {
+				$work->from = date('m/Y', $startDateTime);
+			}
+
+			$endDate = self::getNodeValue($xmlWork, 'EndDate');
+			if (strtolower($endDate) === 'till') {
+				$work->to = 'Hiện tại';
+			} elseif ($endDate) {
+				$endDateTime = self::parseDateString($endDate, '/');
+				if (!$endDateTime) {
+					$endDateTime = self::parseDateString($endDate, '-');
+				}
+				if ($endDateTime) {
+					$work->to = date('m/Y', $endDateTime);
+				}
+			}
+		
 			$candidate->working_list []= $work;
 			$i++;
 		}
@@ -99,5 +138,18 @@ class Parser {
 			$val = strip_tags(trim((string) $xml->$altNode));
 		}
 		return strlen($val) ? $val : null;
+	}
+
+	public static function parseDateString($string, $separator = '/') {
+		$time = strtotime($string);
+		if ($time) {
+			return $time;
+		} else {
+			$arr = explode($separator, $string);
+			if (count($arr) === 3) {
+				return self::parseDateString(implode('-', [$arr[2], $arr[1], $arr[0]]));
+			}
+		}
+		return false;
 	}
 }
